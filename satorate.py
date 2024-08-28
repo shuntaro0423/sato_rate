6ee0c64624b4b0054096c062ed15fb38d4169445
shuntaro0423 <shuntarokik@gmail.com>
2024-08-28 09:50:22 +0900

    Add files via upload

---
+++ satorate.py
@@ -0,0 +1,13 @@
+# 初期値と増加率の定義
+satou_rate = 1.53  # 2022年の佐藤姓の割合
+satou_growth = 1.0083 #佐藤性の増加の割合
+sato_result = 2530 # 佐藤性の割合を計算する年
+
+# 佐藤姓の割合を計算する関数の定義
+def satou(year):
+    return satou_rate * satou_growth ** (year - 2022)
+
+# 例: 2030年の佐藤姓の割合を計算
+result = satou(sato_result)
+print(f"{sato_result}年の佐藤姓の割合: {result}%")
+
